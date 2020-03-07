1e75f323d4ccb4a2bbe4f65641cc08656abcbe9d
austinatkinson93 <austinatkinson93@users.noreply.github.com>
2020-03-07 17:02:15 -0500

    updated connection correctly this time(hopefully)

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host:"ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port:"3306",
-    user:"mutlsvpr0peqnofb",
-    password:"lnqe4ihytrm1ls9x",
-    database:"p7hilx07l3anpdtk"
-})
+let connection;
+
+if(process.env.JAWSDB_URL){
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
+}//if hosted
+else{
+
+    connection = mysql.createConnection({
+        host:"localhost",
+        port:3306,
+        user:"root",
+        password:"root",
+        database:"burgers_db"
+    });
+    
+}//else local
 
 connection.connect(err=>{
     if(err) console.log(err);
